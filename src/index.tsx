a1e8582c3f806bf47f580d70f6fee2c879e38175
xulihang <xulihanghai@163.com>
2024-02-21 13:42:43 +0800

    implement the js definition

--- src/index.tsx
+++ src/index.tsx
@@ -27,8 +27,46 @@ export function multiply(a: number, b: number): Promise<number> {
 /**
  * Crop
  */
-export function crop(frame: Frame): any {
+export function crop(frame: Frame,config?:CropConfig): CropResult {
   'worklet'
   if (plugin == null) throw new Error('Failed to load Frame Processor Plugin "crop"!')
-  return plugin.call(frame) as any;
+  if (config) {
+    let record:Record<string,any> = {};
+    if (config.includeImageBase64 != undefined && config.includeImageBase64 != null) {
+      record["includeImageBase64"] = config.includeImageBase64;
+    }
+    if (config.saveAsFile != undefined && config.saveAsFile != null) {
+      record["saveAsFile"] = config.saveAsFile;
+    }
+    if (config.cropRegion) {
+      let cropRegionRecord:Record<string,any> = {};
+      cropRegionRecord["left"] = config.cropRegion.left;
+      cropRegionRecord["top"] = config.cropRegion.top;
+      cropRegionRecord["width"] = config.cropRegion.width;
+      cropRegionRecord["height"] = config.cropRegion.height;
+      record["cropRegion"] = cropRegionRecord;
+    }
+    return plugin.call(frame,record) as any;
+  }else{
+    return plugin.call(frame) as any;
+  }
+}
+
+//the value is in percentage
+export interface CropRegion{
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+}
+
+export interface CropConfig{
+  cropRegion?: CropRegion;
+  includeImageBase64?: boolean;
+  saveAsFile?: boolean;
+}
+
+export interface CropResult{
+  base64?:string;
+  path?:string;
 }
