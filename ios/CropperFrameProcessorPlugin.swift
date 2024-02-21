95d3cf10895c0e1171f3d9d4c2e27b9ad59ad4f3
xulihang <xulihanghai@163.com>
2024-02-21 17:54:41 +0800

    add ios files

---
+++ ios/CropperFrameProcessorPlugin.swift
@@ -0,0 +1,22 @@
+//
+//  CropperFrameProcessorPlugin.swift
+//  VisionCameraCropper
+//
+//  Created by xulihang on 2024/2/21.
+//
+
+import Foundation
+import VisionCamera
+
+@objc(CropperFrameProcessorPlugin)
+public class CropperFrameProcessorPlugin: FrameProcessorPlugin {
+  public override init(proxy: VisionCameraProxyHolder, options: [AnyHashable : Any]! = [:]) {
+    super.init(proxy: proxy, options: options)
+  }
+
+  public override func callback(_ frame: Frame, withArguments arguments: [AnyHashable : Any]?) -> Any {
+    let buffer = frame.buffer
+    let cropResult: [String:Any] = [:]
+    return cropResult
+  }
+}
---
+++ ios/VisionCameraCropper.h
@@ -0,0 +1,12 @@
+//
+//  VisionCameraCropper.h
+//  Pods
+//
+//  Created by xulihang on 2024/2/21.
+//
+
+#ifndef VisionCameraCropper_h
+#define VisionCameraCropper_h
+
+
+#endif /* VisionCameraCropper_h */
